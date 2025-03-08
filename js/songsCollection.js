64b2d86d3621c3883be96f951120ddc681e73ac4
Brijesh Sharma <brijesharma69@gmail.com>
2025-03-09 01:14:51 +0530

    main: songs-added-alan-walker

--- js/songsCollection.js
+++ js/songsCollection.js
@@ -10,8 +10,23 @@ export const songs = [
         songPart: 'songs/ENG-LI$H SONGS/Alan Walker/Alone (Alan Walker) 320Kbps(Gomirchi.in).mp3'
     },
     {
-        songName: 'On On',
-        artistName: 'Cartoon Ft. Daniel Levi',
+        songName: 'On On - Cartoon Ft. Daniel Levi',
+        artistName: 'Alan Walker',
         songPart: 'songs/ENG-LI$H SONGS/Alan Walker/Cartoon Ft. Daniel Levi - On On (Mp3Goo.com).mp3'
+    },
+    {
+        songName: 'Faded - Live Performance',
+        artistName: 'Alan Walker',
+        songPart: 'songs/ENG-LI$H SONGS/Alan Walker/Faded (Live Performance).mp3'
+    },
+    {
+        songName: 'On My Way',
+        artistName: 'Alan Walker',
+        songPart: 'songs/ENG-LI$H SONGS/Alan Walker/On My Way - Alan Walker 320 Kbps(PagalWorldCom.Com).mp3'
+    },
+    {
+        songName: 'The Spectre',
+        artistName: 'Alan Walker',
+        songPart: 'songs/ENG-LI$H SONGS/Alan Walker/The Spectre - Alan Walker_320-(Mp3Sun.Com).mp3'
     }
 ];
